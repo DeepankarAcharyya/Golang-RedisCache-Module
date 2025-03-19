96fc3ea95215bedab87318b3ffc204740eec460f
Deepankar Acharyya <deepankaracharyya@gmail.com>
2025-03-19 14:41:58 +0530

    updated the parameters

--- cache/cache_conn_config.go
+++ cache/cache_conn_config.go
@@ -2,6 +2,7 @@ package redis_cache
 
 import (
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
@@ -9,32 +10,35 @@ import (
 type CacheConnectionConfig struct {
 	Cache struct {
 		Usage_Cache_DB struct {
-			Host                   string `yaml:"host"`
-			Port                   string `yaml:"port"`
-			Password               string `yaml:"password"`
-			Database               string `yaml:"database"`
-			Pool_Max_Connections   int    `yaml:"pool_max_connections"`
-			Pool_Min_Connections   int    `yaml:"pool_min_connections"`
-			Auto_Pipelining_Mode   bool   `yaml:"auto_pipelining_mode"`
-			DisableClientSideCache bool   `yaml:"disable_cache"`
-			Pool_Max_Idle_Time     string `yaml:"pool_max_idle_time"`
+			Host                   string        `yaml:"host"`
+			Port                   string        `yaml:"port"`
+			Password               string        `yaml:"password"`
+			Database               int           `yaml:"database"`
+			Pool_Max_Connections   int           `yaml:"pool_max_connections"`
+			Pool_Min_Connections   int           `yaml:"pool_min_connections"`
+			Auto_Pipelining_Mode   bool          `yaml:"auto_pipelining_mode"`
+			DisableClientSideCache bool          `yaml:"disable_cache"`
+			Pool_Max_Idle_Time     time.Duration `yaml:"pool_max_idle_time"`
 		} `yaml:"usage_cache_db"`
 	} `yaml:"cache"`
 }
 
 // setDefaults sets default values for fields that are empty
 func (c *CacheConnectionConfig) setDefaults() {
+	if c.Cache.Usage_Cache_DB.Host == "" {
+		c.Cache.Usage_Cache_DB.Host = "127.0.0.1"
+	}
 	if c.Cache.Usage_Cache_DB.Port == "" {
 		c.Cache.Usage_Cache_DB.Port = "6379"
 	}
-	if c.Cache.Usage_Cache_DB.Database == "" {
-		c.Cache.Usage_Cache_DB.Database = "0"
-	}
-	if c.Cache.Usage_Cache_DB.SSL_Mode == "" {
-		c.Cache.Usage_Cache_DB.SSL_Mode = "disable"
-	}
 	if !c.Cache.Usage_Cache_DB.Auto_Pipelining_Mode {
 		c.Cache.Usage_Cache_DB.Auto_Pipelining_Mode = false
+	}
+	if !c.Cache.Usage_Cache_DB.DisableClientSideCache {
+		c.Cache.Usage_Cache_DB.DisableClientSideCache = true
+	}
+	if c.Cache.Usage_Cache_DB.Pool_Max_Idle_Time == 0 {
+		c.Cache.Usage_Cache_DB.Pool_Max_Idle_Time = 90 * 60 * time.Second
 	}
 }
 
